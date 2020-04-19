5fa12572763c91fb8547fc7f886ecce307af6868
Xiaoyi Liu <lxy878@gmail.com>
2020-04-19 09:26:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I build"
+    "Welcome to your app!!!! Adding A New Text!!!!"
   end
 
 end
